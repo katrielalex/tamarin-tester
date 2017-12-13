97815b6b3e8abcc4d2f472c25f52f6097fde2d44
Katriel Cohn-Gordon <me@katriel.co.uk>
2017-12-13 14:42:22 +0000

    autopep8

--- src/pathtype.py
+++ src/pathtype.py
@@ -1,7 +1,9 @@
-#Stolen from https://stackoverflow.com/questions/11415570/directory-path-types-with-argparse
+# Stolen from
+# https://stackoverflow.com/questions/11415570/directory-path-types-with-argparse
 
 from argparse import ArgumentTypeError as err
 import os
+
 
 class PathType(object):
     def __init__(self, exists=True, type='file', dash_ok=True):
@@ -14,42 +16,45 @@ class PathType(object):
            dash_ok: whether to allow "-" as stdin/stdout'''
 
         assert exists in (True, False, None)
-        assert type in ('file','dir','symlink',None) or hasattr(type,'__call__')
+        assert type in ('file', 'dir', 'symlink',
+                        None) or hasattr(type, '__call__')
 
         self._exists = exists
         self._type = type
         self._dash_ok = dash_ok
 
     def __call__(self, string):
-        if string=='-':
+        if string == '-':
             # the special argument "-" means sys.std{in,out}
             if self._type == 'dir':
-                raise err('standard input/output (-) not allowed as directory path')
+                raise err(
+                    'standard input/output (-) not allowed as directory path')
             elif self._type == 'symlink':
-                raise err('standard input/output (-) not allowed as symlink path')
+                raise err(
+                    'standard input/output (-) not allowed as symlink path')
             elif not self._dash_ok:
                 raise err('standard input/output (-) not allowed')
         else:
             e = os.path.exists(string)
-            if self._exists==True:
+            if self._exists == True:
                 if not e:
                     raise err("path does not exist: '%s'" % string)
 
                 if self._type is None:
                     pass
-                elif self._type=='file':
+                elif self._type == 'file':
                     if not os.path.isfile(string):
                         raise err("path is not a file: '%s'" % string)
-                elif self._type=='symlink':
+                elif self._type == 'symlink':
                     if not os.path.symlink(string):
                         raise err("path is not a symlink: '%s'" % string)
-                elif self._type=='dir':
+                elif self._type == 'dir':
                     if not os.path.isdir(string):
                         raise err("path is not a directory: '%s'" % string)
                 elif not self._type(string):
                     raise err("path not valid: '%s'" % string)
             else:
-                if self._exists==False and e:
+                if self._exists == False and e:
                     raise err("path exists: '%s'" % string)
 
                 p = os.path.dirname(os.path.normpath(string)) or '.'
